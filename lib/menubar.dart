094c15a787b564967d3df24b3c7e1735cf742774
Vvubya_Drake <vvubyadrake7@gmail.com>
2023-08-21 15:03:19 +0000

    modified

--- lib/menubar.dart
+++ lib/menubar.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import FirebaseAuth
 import 'login.dart';
 import 'userprofile.dart';
 import 'signup.dart';
 import 'emergency_contacts.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: MenuBarScreen(),
-    );
-  }
-}
 
 class MenuBarScreen extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -44,107 +29,92 @@ class CustomMenuBar extends StatefulWidget {
 }
 
 class _MenuBarState extends State<CustomMenuBar> {
-  User? currentUser;
-  late Future<DocumentSnapshot> userSnapshot;
+  String? userId; // Define userId variable
+  String? displayName; // Define displayName variable
 
   @override
   void initState() {
     super.initState();
-    currentUser = widget.auth.currentUser;
-    userSnapshot = getUserInfo(currentUser?.uid);
-  }
-
-  Future<DocumentSnapshot> getUserInfo(String? userId) async {
-    if (userId != null) {
-      return FirebaseFirestore.instance.collection('users').doc(userId).get();
+    // Get the current user's ID and display name when the menu bar is created
+    if (widget.auth.currentUser != null) {
+      userId = widget.auth.currentUser!.uid;
+      displayName = widget.auth.currentUser!.displayName;
     }
-    throw Exception("User ID is null");
   }
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: FutureBuilder<DocumentSnapshot>(
-        future: userSnapshot,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error loading user data');
-          } else {
-            var displayName = snapshot.data?['displayName'] ?? 'User Name';
-            return Column(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  color: Colors.blue,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CircleAvatar(
-                          radius: 50,
-                          // Display user profile image here
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          displayName,
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        ),
-                      ],
-                    ),
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.3,
+            color: Colors.blue,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    // Display user profile image here
                   ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text("Profile"),
-                  onTap: () {
-                    if (currentUser?.uid != null) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) {
-                            return ProfileScreen(userId: currentUser!.uid);
-                          },
-                        ),
-                      );
-                    }
+                  SizedBox(height: 10),
+                  Text(
+                    displayName ??
+                        "User Name", // Display the displayName or "User Name" if null
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Profile"),
+            onTap: () {
+              if (userId != null) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return ProfileScreen(userId: userId!);
+                    },
+                  ),
+                );
+              }
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text("Logout"),
+            onTap: () async {
+              await widget.auth.signOut();
+              Navigator.of(context).pop();
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => LoginScreen(),
+              ));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text("Notifications"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text("Emergency Contacts"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext content) {
+                    return EmergencyContactsScreen();
                   },
                 ),
-                ListTile(
-                  leading: Icon(Icons.logout),
-                  title: Text("Logout"),
-                  onTap: () async {
-                    await widget.auth.signOut();
-                    Navigator.of(context).pop();
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => LoginScreen(),
-                    ));
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.notifications),
-                  title: Text("Notifications"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.phone),
-                  title: Text("Emergency Contacts"),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext content) {
-                          return EmergencyContactsScreen();
-                        },
-                      ),
-                    );
-                  },
-                ),
-              ],
-            );
-          }
-        },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
